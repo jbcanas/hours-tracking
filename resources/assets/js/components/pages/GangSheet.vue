9efc56ff21b685e5665c9ed603547f64a2de1e11
Jay Bryan Cañas <jbryan.canas@gmail.com>
2017-07-24 00:22:25 +0800

    adjusted the layout of gangsheet form

--- resources/assets/js/components/pages/GangSheet.vue
+++ resources/assets/js/components/pages/GangSheet.vue
@@ -7,190 +7,142 @@
             <div class="portlet light">
                 <div class="portlet-title">
                     <div class="actions pull-left">
-                        <a href="javascript:;" class="btn btn-primary">
+                        <a href="javascript:;" class="btn blue">
                             <i class="fa fa-plus"></i> New</a>
-                        <a href="javascript:;" class="btn btn-primary">
+                        <a href="javascript:;" class="btn blue">
                             <i class="fa fa-search"></i> Find </a>
                         <a href="javascript:;" class="btn btn-danger">
                             <i class="fa fa-minus"></i> Cancel </a>
                     </div>
                     <div class="actions pull-right">
-                        <a href="javascript:;" class="btn btn-primary">
-                            <i class="fa fa-external-link"></i> Submit</a>
+                        <a href="javascript:;" class="btn blue-steel">
+                            <i class="fa fa-external-link"></i> Save</a>
                     </div>
                 </div>
                 <div class="portlet-body">
-                    <form action="#" class="horizontal-form">
+                    <form action="#" class="form-horizontal gangSheetForm">
                         <div class="form-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Account Description</label>
-                                        <div class="col-md-9">
-                                            <job-type type="accountDescription"></job-type>
-                                        </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Account Description</label>
+                                    <div class="col-md-7">
+                                        <job-type type="accountDescription"></job-type>
                                     </div>
                                 </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Requested by</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">{{ jobNameLabel }}</label>
+                                    <div class="col-md-7">
+                                        <job-type type="jobName"></job-type>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Arbitration Award</label>
+                                    <div class="col-md-9">
+                                        <label class="mt-checkbox">
+                                            <input type="checkbox" />
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Vessel/Barge</label>
+                                    <div class="col-md-5">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Voyage</label>
+                                    <div class="col-md-5">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Job Sheet Number</label>
+                                    <div class="col-md-5">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">ILWU Job Number</label>
+                                    <div class="col-md-5">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Notes</label>
+                                    <div class="col-md-9">
+                                        <textarea class="form-control" cols="30" rows="3"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Requested by</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Request Date</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Work Date</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Start Time</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Stop Time</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Meal Break</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Cofee Break</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Early Start</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Moves</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="control-label col-md-3">Gang Shift</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control">
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">{{ jobNameLabel }}</label>
-                                        <div class="col-md-9">
-                                            <job-type type="jobName"></job-type>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Request Date</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Arbitration Award</label>
-                                        <div class="col-md-9">
-                                            <label class="mt-checkbox">
-                                                <input type="checkbox" />
-                                                <span></span>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Work Date</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Vessel/Barge</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Start Time</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Voyage</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Stop Time</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Job Sheet Number</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Meal Break</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">ILWU Job Number</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Cofee Break</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Notes</label>
-                                        <div class="col-md-9">
-                                            <textarea class="form-control" cols="30" rows="3"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Early Start</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Moves</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3">Gang Shift</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                
-                                </div>
+                            <div class="col-md-12">
+                                <gangsheet-employees></gangsheet-employees>
                             </div>
                            
-                            <gangsheet-employees></gangsheet-employees>
                         </div>
                     </form>
                 </div>
